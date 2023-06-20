d0d9f8f74af3336f569ab2bced0381a37ad5ece5
RusoCop911 <130517846+RusoCop911@users.noreply.github.com>
2023-06-20 15:43:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,65 +9,73 @@ let divisa = [
     {id:4, nombre:'yen', continente:'asia', valorBlue:347}
 ]
 
+let respuestaContinente = ''
+
 do {
-    respuesta = prompt('De qué continente desea buscar divisas?')
-    if (respuesta == 'america' || 'américa') {
-        alert(divisa[1])
+    respuestaContinente = prompt('De qué continente desea buscar divisas? \n (América - Europa - Asia) \n para salir tipear "salir" o click en "Cancel"').toLowerCase()
+    const continenteElegido = divisa.find(divisa=> divisa.continente === respuestaContinente)
+    alert(continenteElegido.respuestaDivisa)
+} while(respuestaContinente !== 'salir')
+
+let respuestaDivisa = ''
+
+do {
+    respuestaDivisa = prompt('Qué desea convertir? \n 1) Dólares \n 2) Reales \n Click en "cancel" para volver').toLowerCase()
+    const divisaElegida = divisa.find(divisa=> divisa.nombre === respuestaDivisa)
+    alert(divisaElegida.validarDatos())
+} while(respuestaDivisa !== 'volver')
+
+function validarDatos() {
+    let intentos = 0
+    let cambio 
+
+    while (intentos < 3) {
+
+        cambio = prompt('Qué desea convertir? \n (1 U$D = $500) \n (D) Dólares americanos a pesos argentinos \n (P) Pesos argentinos a dólares americanos').toUpperCase()
+
+        if (cambio === "D") {
+            cambioDolar()
+        } else if (cambio === "P") {
+            cambioPesos()
+        } else {
+            alert('ingrese solo "D" o "P" sin comillas')
+            intentos++
+        }
+    }
+
+    if (intentos === 3) {
+        alert('La "D" está junto a la "S" y la "P" está pegada a la "O", no es tan difícil')
+        validarDatos()
+    }
+}
+
+function cambioDolar() {
+
+    let dolar = Number(prompt('Ingrese la cantidad de dólares sin decimales o "0" para voler al menú'))
+
+    if (isNaN(dolar)) {
+        alert('ingrese cifra numerica')
+        cambioDolar()
+    } else if (dolar === 0) {
+        validarDatos()
+    } else {
+        alert('La cantidad en pesos seria de' + ' ' + (dolar * 500))
     }
 }
 
 
-// function validarDatos() {
-//     let intentos = 0
-//     let cambio 
+function cambioPesos() {
 
-//     while (intentos < 3) {
+    let pesos = Number(prompt('Ingrese la cantidad de pesos sin decimales o "0" para voler al menú'))
 
-//         cambio = prompt('Qué desea convertir? \n (1 U$D = $500) \n (D) Dólares americanos a pesos argentinos \n (P) Pesos argentinos a dólares americanos').toUpperCase()
-
-//         if (cambio === "D") {
-//             cambioDolar()
-//         } else if (cambio === "P") {
-//             cambioPesos()
-//         } else {
-//             alert('ingrese solo "D" o "P" sin comillas')
-//             intentos++
-//         }
-//     }
-
-//     if (intentos === 3) {
-//         alert('La "D" está junto a la "S" y la "P" está pegada a la "O", no es tan difícil')
-//         validarDatos()
-//     }
-// }
-
-// function cambioDolar() {
-
-//     let dolar = Number(prompt('Ingrese la cantidad de dólares sin decimales o "0" para voler al menú'))
-
-//     if (isNaN(dolar)) {
-//         alert('ingrese cifra numerica')
-//         cambioDolar()
-//     } else if (dolar === 0) {
-//         validarDatos()
-//     } else {
-//         alert('La cantidad en pesos seria de' + ' ' + (dolar * 500))
-//     }
-// }
+    if (isNaN(pesos)) {
+        alert('ingrese cifra numerica')
+        cambioPesos()
+    } else if (pesos === 0) {
+        validarDatos()
+    } else {
+        alert('La cantidad en dolar blue seria de' + ' ' + (pesos / 500))
+    }
+}
 
 
-// function cambioPesos() {
-
-//     let pesos = Number(prompt('Ingrese la cantidad de pesos sin decimales o "0" para voler al menú'))
-
-//     if (isNaN(pesos)) {
-//         alert('ingrese cifra numerica')
-//         cambioPesos()
-//     } else if (pesos === 0) {
-//         validarDatos()
-//     } else {
-//         alert('La cantidad en dolar blue seria de' + ' ' + (pesos / 500))
-//     }
-// }
-
-// validarDatos()
