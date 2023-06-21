e1210e679d86763e233770dc6031a2522e434fe9
RusoCop911 <130517846+RusoCop911@users.noreply.github.com>
2023-06-21 13:01:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 let nombre = prompt('Ingrese su Nombre')
 let apellido = prompt('Ingrese su Apellido')
-alert('Bienvenido/a ' + nombre + ' ' + apellido + ' a la calculadora de divisas')
+alert('Bienvenido/a ' + nombre + ' ' + apellido + ' a la calculadora de divisas \nAFIP le agradece sus datos. ')
 
 let divisa = [
-    {id:1, nombre:'dolares', continente:'america', pais:'eeuu', valorBlue:500},
-    {id:2, nombre:'euro', continente:'europa', pais:'union europea', valorBlue:540},
-    {id:3, nombre:'real', continente:'america', pais:'brasil', valorBlue:91},
-    {id:4, nombre:'yen', continente:'asia', pais:'japon', valorBlue:347}
+    { id: "1", nombre: 'dolares', continente: 'américa', pais: 'eeuu', valorBlue: 500 },
+    { id: "2", nombre: 'euro', continente: 'europa', pais: 'union europea', valorBlue: 540 },
+    { id: "3", nombre: 'real', continente: 'américa', pais: 'brasil', valorBlue: 91 }
 ]
 
 let respuestaContinente = ''
 
-do {
-    respuestaContinente = prompt('De qué continente desea buscar divisas? \n 1) América \n 2) Europa \n 3) Asia \n Para salir tipear "salir" o click en "Cancel"').toLowerCase()
-    const continenteElegido = divisa.find(divisa => divisa.continente === respuestaContinente)
-    
+
+
+function seleccionContinente() {
+    respuestaContinente = prompt('De qué continente desea buscar divisas? \n 1) América \n 2) Europa \n Para salir tipear "salir" o click en "Cancel"').toLowerCase()
+    let continenteElegido = divisa.find(divisa => divisa.id === respuestaContinente)
+
     if (continenteElegido) {
-        if (continenteElegido.continente === 'america' || continenteElegido.continente === 'américa' || continenteElegido.continente === '1') {
-            const seleccionDivisa = prompt('Selecciona la divisa que deseas convertir a pesos argentinos: \n 1) Dólares \n 2) Reales \n Click en "Cancel" para volver').toLowerCase()
+        if (continenteElegido.id === "1") {
+            let seleccionDivisa = prompt('Selecciona la divisa que deseas convertir a pesos argentinos: \n 1) Dólares \n 2) Reales \n Click en "Cancel" para volver').toLowerCase()
             if (seleccionDivisa === '1' || seleccionDivisa === 'dólares' || seleccionDivisa === 'dolares') {
                 validarDatosDolares()
             } else if (seleccionDivisa === '2' || seleccionDivisa === 'reales') {
@@ -25,17 +26,45 @@ do {
             } else {
                 alert('Opción inválida. Por favor, elija una opción válida.')
             }
-        } else {
-            alert(continenteElegido.respuestaDivisa)
+        } else if (continenteElegido.id === "2") {
+            validarDatosEuro()
+        } else if (continenteElegido.id === "3") {
+            validarDatosYen()
         }
     } else {
         alert('Continente no encontrado. Por favor, ingrese un continente válido.')
     }
-} while (respuestaContinente !== 'salir')
+}
+
+
+
+// do {
+//     respuestaContinente = prompt('De qué continente desea buscar divisas? \n (Tipee respetando los signos de puntución) \n 1) América \n 2) Europa \n Para salir tipear "salir" o click en "Cancel"').toLowerCase()
+//     let continenteElegido = divisa.find(divisa => divisa.id === respuestaContinente)
+
+//     if (continenteElegido) {
+//         if (continenteElegido.id === 1) {
+//             const seleccionDivisa = prompt('Selecciona la divisa que deseas convertir a pesos argentinos: \n 1) Dólares \n 2) Reales \n Click en "Cancel" para volver').toLowerCase()
+//             if (seleccionDivisa === '1' || seleccionDivisa === 'dólares' || seleccionDivisa === 'dolares') {
+//                 validarDatosDolares()
+//             } else if (seleccionDivisa === '2' || seleccionDivisa === 'reales') {
+//                 validarDatosReales()
+//             } else {
+//                 alert('Opción inválida. Por favor, elija una opción válida.')
+//             }
+//         } else if (continenteElegido.id === 2) {
+//             validarDatosEuro()
+//         } else if (continenteElegido.id === 3) {
+
+//         }
+//     } else {
+//         alert('Continente no encontrado. Por favor, ingrese un continente válido.')
+//     }
+// } while (respuestaContinente !== 'salir')
 
 function validarDatosDolares() {
     let intentos = 0
-    let cambio 
+    let cambio
 
     while (intentos < 3) {
 
@@ -67,7 +96,7 @@ function cambioDolar() {
     } else if (dolar === 0) {
         validarDatosDolares()
     } else {
-        alert('La cantidad en pesos seria de ' + (dolar * 500))
+        alert('La cantidad en pesos sería de ' + (dolar * 500))
     }
 }
 
@@ -79,15 +108,15 @@ function cambioPesos() {
         alert('ingrese cifra numerica')
         cambioPesos()
     } else if (pesos === 0) {
-        validarDatosDolares()
+        validarDatos()
     } else {
-        alert('La cantidad en ' + continenteElegido.nombre + 'blue seria de ' + (pesos / continenteElegido.valorBlue))
+        alert('La cantidad en dolar blue sería de' + ' ' + (pesos / 500))
     }
 }
 
 function validarDatosReales() {
     let intentos = 0
-    let cambio 
+    let cambio
 
     while (intentos < 3) {
 
@@ -96,7 +125,7 @@ function validarDatosReales() {
         if (cambio === "R") {
             cambioReal()
         } else if (cambio === "P") {
-            cambioPesos()
+            cambioPesosReales()
         } else {
             alert('ingrese solo "R" o "P" sin comillas')
             intentos++
@@ -111,14 +140,85 @@ function validarDatosReales() {
 
 function cambioReal() {
 
-    let Real = Number(prompt('Ingrese la cantidad de Reales sin decimales o "0" para voler al menú'))
+    let real = Number(prompt('Ingrese la cantidad de Reales sin decimales o "0" para voler al menú'))
 
-    if (isNaN(Real)) {
+    if (isNaN(real)) {
         alert('ingrese cifra numerica')
         cambioReal()
-    } else if (Real === 0) {
+    } else if (real === 0) {
         validarDatosReales()
     } else {
-        alert('La cantidad en pesos seria de' + ' ' + (Real * 91))
+        alert('La cantidad en pesos sería de ' + (real * 91))
     }
+}
+
+function cambioPesosReales() {
+
+    let pesos = Number(prompt('Ingrese la cantidad de pesos sin decimales o "0" para voler al menú'))
+
+    if (isNaN(pesos)) {
+        alert('ingrese cifra numerica')
+        cambioPesosReales()
+    } else if (pesos === 0) {
+        validarDatos()
+    } else {
+        alert('La cantidad en Real Blue sería de' + ' ' + (pesos / 91))
+    }
+}
+
+function validarDatosEuro() {
+    let intentos = 0
+    let cambio
+
+    while (intentos < 3) {
+
+        cambio = prompt('Qué desea convertir? \n (1 Euro = $540) \n (E) Euros a pesos argentinos \n (P) Pesos argentinos a Euros').toUpperCase()
+
+        if (cambio === "E") {
+            cambioEuro()
+        } else if (cambio === "P") {
+            cambioPesosEuros()
+        } else {
+            alert('ingrese solo "E" o "P" sin comillas')
+            intentos++
+        }
+    }
+
+    if (intentos === 3) {
+        alert('La "E" está junto a la "R" y la "P" está pegada a la "O", no es tan difícil')
+        validarDatosDolares()
+    }
+}
+
+function cambioEuro() {
+
+    let euro = Number(prompt('Ingrese la cantidad de Euros sin decimales o "0" para voler al menú'))
+
+    if (isNaN(euro)) {
+        alert('ingrese cifra numerica')
+        cambioEuro()
+    } else if (euro === 0) {
+        validarDatosEuro()
+    } else {
+        alert('La cantidad en pesos sería de ' + (euro * 540))
+    }
+}
+
+function cambioPesosEuros() {
+
+    let pesos = Number(prompt('Ingrese la cantidad de pesos sin decimales o "0" para voler al menú'))
+
+    if (isNaN(pesos)) {
+        alert('ingrese cifra numerica')
+        cambioPesosEuros()
+    } else if (pesos === 0) {
+        validarDatos()
+    } else {
+        alert('La cantidad en Euro Blue sería de' + ' ' + (pesos / 540))
+    }
+}
+
+function validarDatosYen() {
+    alert("Disculpe, por el momento esta función no se encuentra disponible")
+    seleccionContinente()
 }
